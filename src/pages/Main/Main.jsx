080ab03b7e0a1d00b07b867452459df8ae0ecede
Roman <aromawriter@mail.ru>
2023-05-27 16:18:00 +0300

    work in progress

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import { Header } from "../../components/Header/Header";
 import { UserItem } from "../../components/UserItem/UserItem";
 import s from "./Main.module.css";
 
 export const Main = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
   
   return (
     <div className={s.main}>
-      <Header setLogin={setLogin} setPassword={setPassword}/>
-      <UserItem login={login}/>
+      <Header/>
+      <UserItem/>
     </div>
   );
 };
